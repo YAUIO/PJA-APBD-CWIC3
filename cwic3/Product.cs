d00c0be3a68b7240a7943469ff5c13f8e5366e0e
Artiom Bezkorovainyi <s30174@pjwstk.edu.pl>
2025-03-23 11:29:09 +0100

    rewrote using readonly added ToString

--- cwic3/Product.cs
+++ cwic3/Product.cs
@@ -3,12 +3,20 @@
 public record Product
 {
     private static int _counter;
-    public int Id { get; }
-    public required string Name { set; get; }
-    public required double Temperature { set; get; }
 
-    public Product()
+    public readonly int Id;
+    public readonly string Name;
+    public readonly double Temperature;
+
+    public Product(string name, double temp)
     {
         Id = _counter++;
+        Name = name;
+        Temperature = temp;
+    }
+
+    public override string ToString()
+    {
+        return "Product " + Name + ", Temp.Required " + Temperature;
     }
 }
