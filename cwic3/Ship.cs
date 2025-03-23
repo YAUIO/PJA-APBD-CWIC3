c0741ffe6c3ab38c8b10ac459741b7bd22d9d476
Artiom Bezkorovainyi <s30174@pjwstk.edu.pl>
2025-03-23 03:31:07 +0100

    ship class according to requirements

---
+++ cwic3/Ship.cs
@@ -0,0 +1,68 @@
+﻿using cwic3.Containers;
+
+namespace cwic3;
+
+public class Ship
+{
+    private List<BaseContainer> _containers = [];
+
+    public Ship(double maxWeight, int maxAmount, double maxSpeed)
+    {
+        MaxWeight = maxWeight;
+        MaxAmount = maxAmount;
+        MaxSpeed = maxSpeed;
+    }
+
+    public double MaxSpeed { get; }
+    public int MaxAmount { get; }
+    public double MaxWeight { get; }
+
+    public void Load(BaseContainer container)
+    {
+        _containers.Add(container);
+    }
+    
+    public void Load(List<BaseContainer> containers)
+    {
+        _containers.AddRange(containers);
+    }
+
+    public bool Remove(BaseContainer container)
+    {
+        return _containers.Remove(container);
+    }
+
+    public bool Unload(BaseContainer container)
+    {
+        return _containers.Remove(container);
+    }
+
+    public bool Replace(string serialNumber, BaseContainer replacement)
+    {
+        BaseContainer? container = null;
+        foreach (var bc in _containers)
+        {
+            if (bc.SerialNumber.Equals(serialNumber))
+            {
+                container = bc;
+                break;
+            }
+        }
+
+        if (container == null) return false;
+
+        _containers.Insert(_containers.IndexOf(container), replacement);
+        return _containers.Remove(container);
+    }
+
+    public override string ToString()
+    {
+        return 
+            "Ship info: \n" +
+               "MaxWeight: " + MaxWeight + " \n" +
+               "MaxAmount: " + MaxAmount + " \n" +
+               "MaxSpeed: " + MaxSpeed + " \n" +
+               "Containers: " + _containers + " \n"
+                    ;
+    }
+}
