335b4729ce8b050915dfc3cc2a53db6a343e1b86
Artiom Bezkorovainyi <s30174@pjwstk.edu.pl>
2025-03-23 12:04:00 +0100

    Rewrote unload to unload cargo from the container

--- cwic3/Ship.cs
+++ cwic3/Ship.cs
@@ -1,4 +1,6 @@
-﻿using cwic3.Containers;
+﻿using System.Text;
+using cwic3.Containers;
+using cwic3.Exceptions;
 
 namespace cwic3;
 
@@ -17,14 +19,29 @@ public class Ship
         MaxSpeed = maxSpeed;
     }
 
+    private static int GetTotalMass(List<BaseContainer> containers)
+    {
+        var mass = 0;
+        containers.ForEach(c => mass += c.Mass);
+        return mass / 1000; //conversion form kg to t
+    }
+
+    private double GetTotalMass()
+    {
+        return GetTotalMass(_containers);
+    }
+
     public void Load(BaseContainer container)
     {
+        if (_containers.Count == MaxAmount) throw new OverfillException("Max container capacity reached");
+        if (GetTotalMass() + container.Mass > MaxWeight) throw new OverfillException("Loaded mass exceeds max amount");
         _containers.Add(container);
     }
 
     public void Load(List<BaseContainer> containers)
     {
-        _containers.AddRange(containers);
+        foreach (var c in containers) //so we don't need to repeat all check logic here
+            Load(c);
     }
 
     public bool Remove(BaseContainer container)
@@ -34,7 +51,18 @@ public class Ship
 
     public bool Unload(BaseContainer container)
     {
-        return _containers.Remove(container);
+        if (_containers.Contains(container))
+        {
+            var ind = _containers.IndexOf(container);
+            _containers.Remove(container);
+            container.Empty();
+            _containers.Insert(ind, container);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
     public bool Replace(string serialNumber, BaseContainer replacement)
@@ -55,11 +83,20 @@ public class Ship
 
     public override string ToString()
     {
+        StringBuilder sb = new StringBuilder();
+        sb.Append("{ \n");
+        foreach (BaseContainer c in _containers)
+        {
+            sb.Append(c).Append("\n\n");
+        }
+        if (sb.Length > 0) sb.Remove(sb.Length - 2, 2);
+        sb.Append(" }");
+        
         return
             "Ship info: \n" +
             "MaxWeight: " + MaxWeight + " \n" +
             "MaxAmount: " + MaxAmount + " \n" +
             "MaxSpeed: " + MaxSpeed + " \n" +
-            "Containers: " + _containers;
+            "Containers: " + sb;
     }
 }
